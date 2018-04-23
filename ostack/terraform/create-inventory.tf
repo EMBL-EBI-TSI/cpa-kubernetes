4979d14f1042daa241f58ead54bfd04a7ebe0bcd
Gianni Dalla Torre <gianni@ebi.ac.uk>
2018-04-23 16:57:58 +0100

    Ansible `inventory.ini` in the deployment folder

--- ostack/terraform/create-inventory.tf
+++ ostack/terraform/create-inventory.tf
@@ -13,32 +13,32 @@ resource "null_resource" "ansible-provision" {
 
   ##Create Masters Inventory
   provisioner "local-exec" {
-    command = "echo \"[kube-master]\n${openstack_compute_instance_v2.k8s-master.name} ansible_ssh_host=${openstack_networking_floatingip_v2.floatingip.address}\" > ostack/ansible/inventory.ini"
+    command = "echo \"[kube-master]\n${openstack_compute_instance_v2.k8s-master.name} ansible_ssh_host=${openstack_networking_floatingip_v2.floatingip.address}\" > $${TF_VAR_deployment_path}/inventory.ini"
   }
 
   ##Create ETCD Inventory
   provisioner "local-exec" {
-    command = "echo \"\n[etcd]\n${openstack_compute_instance_v2.k8s-master.name} ansible_ssh_host=${openstack_networking_floatingip_v2.floatingip.address}\" >> ostack/ansible/inventory.ini"
+    command = "echo \"\n[etcd]\n${openstack_compute_instance_v2.k8s-master.name} ansible_ssh_host=${openstack_networking_floatingip_v2.floatingip.address}\" >> $${TF_VAR_deployment_path}/inventory.ini"
   }
 
   provisioner "local-exec" {
-    command = "echo \"\n[bastion]\" >> ostack/ansible/inventory.ini"
+    command = "echo \"\n[bastion]\" >> $${TF_VAR_deployment_path}/inventory.ini"
   }
 
   provisioner "local-exec" {
-    command = "echo \"bastion ansible_ssh_host=${openstack_networking_floatingip_v2.floatingip.address}\" >> ostack/ansible/inventory.ini"
+    command = "echo \"bastion ansible_ssh_host=${openstack_networking_floatingip_v2.floatingip.address}\" >> $${TF_VAR_deployment_path}/inventory.ini"
   }
 
   ##Create Nodes Inventory
   provisioner "local-exec" {
-    command = "echo \"\n[kube-node]\" >> ostack/ansible/inventory.ini"
+    command = "echo \"\n[kube-node]\" >> $${TF_VAR_deployment_path}/inventory.ini"
   }
 
   provisioner "local-exec" {
-    command = "echo \"${join("\n",formatlist("%s ansible_ssh_host=%s", openstack_compute_instance_v2.k8s-node.*.name, openstack_compute_instance_v2.k8s-node.*.access_ip_v4))}\" >> ostack/ansible/inventory.ini"
+    command = "echo \"${join("\n",formatlist("%s ansible_ssh_host=%s", openstack_compute_instance_v2.k8s-node.*.name, openstack_compute_instance_v2.k8s-node.*.access_ip_v4))}\" >> $${TF_VAR_deployment_path}/inventory.ini"
   }
 
   provisioner "local-exec" {
-    command = "echo \"\n[k8s-cluster:children]\nkube-node\nkube-master\" >> ostack/ansible/inventory.ini"
+    command = "echo \"\n[k8s-cluster:children]\nkube-node\nkube-master\" >> $${TF_VAR_deployment_path}/inventory.ini"
   }
 }
